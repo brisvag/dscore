062ec34445b7da9920337d8a181c4d351ccd2f26
Lorenzo Gaifas <brisvag@gmail.com>
2021-04-08 18:48:33 +0200

    added residue column to result

--- dscore/main.py
+++ dscore/main.py
@@ -8,7 +8,8 @@ from .servers import get_functions
 async def event_loop(seq):
     tasks = [asyncio.create_task(func(seq)) for func in get_functions.values()]
     results = await asyncio.gather(*tasks)
-    merged = pd.concat(results, axis=1)
+    seq_column = pd.DataFrame({'residue': list(seq)})
+    merged = pd.concat([seq_column] + results, axis=1)
     return merged
 
 
