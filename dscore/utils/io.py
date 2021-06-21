2e9fe515cdf69584099214c89f2bb7174e0351ad
Lorenzo Gaifas <brisvag@gmail.com>
2021-06-21 11:59:46 +0200

    reading old results

--- dscore/utils/io.py
+++ dscore/utils/io.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import pandas as pd
+import re
 
 
 def save_file(text, path):
@@ -8,3 +10,24 @@ def save_file(text, path):
         path.rename(bak)
     with open(path, 'w+') as f:
         f.write(text)
+
+
+def read_dscore(dscore_or_csv):
+    path = Path(dscore_or_csv)
+    with open(dscore_or_csv, 'r') as f:
+        if path.suffix == '.dscore':
+            columns = []
+            while True:
+                line = f.readline()
+                if match := re.match('# \d+\. (.*)$', line):
+                    columns.append(match.group(1))
+                elif line.startswith('-'):
+                    break
+            df = pd.read_csv(f, sep='\s+', names=columns)
+            df.iloc[:, 2:] = df.iloc[:, 2:].replace('-', False)
+            df.iloc[:, 2:] = df.iloc[:, 2:].replace('D', True)
+        else:
+            df = pd.read_csv(path, sep=' ')
+            df.iloc[:, 2:] = df.iloc[:, 2:].replace(0, False)
+            df.iloc[:, 2:] = df.iloc[:, 2:].replace(1, True)
+    return df
